0559a0298065843774a9371367cf088353461db3
TimeEscaper <rentgeny05@gmail.com>
2017-04-14 00:30:44 +0300

    DBFlow init

--- app/src/main/java/ru/mail/tp/perfecture/App.java
+++ app/src/main/java/ru/mail/tp/perfecture/App.java
@@ -2,9 +2,17 @@ package ru.mail.tp.perfecture;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.raizlabs.android.dbflow.config.FlowManager;
+
 /**
  * Created by maksimus on 01.04.17.
  */
 
 public class App extends MultiDexApplication{
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FlowManager.init(this);
+    }
 }
